5b07b74d1b321ff1672eb68d122185a45be764d5
José Miguel Ramos <miguelyinyang@icloud.com>
2026-05-28 11:49:58 +0200

    create add post

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,6 +13,41 @@ POSTS = [
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+    data = request.json
+
+    missing_fields = []
+
+    if not data.get('title'):
+        missing_fields.append('title')
+
+    if not data.get('content'):
+        missing_fields.append('content')
+
+    if missing_fields:
+        return jsonify(
+            {'error': 'Invalid post', 'missing fields': missing_fields}), 400
+
+    if POSTS:
+        new_id = max(post['id'] for post in POSTS) + 1
+    else:
+        new_id = 1
+
+    title = data.get('title')
+    content = data.get('content')
+
+    new_post = {
+        'id': new_id,
+        'title': title,
+        'content': content
+    }
+
+    POSTS.append(new_post)
+
+    return jsonify(new_post), 201
 
 
 if __name__ == '__main__':
